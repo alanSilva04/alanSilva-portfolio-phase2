ccc2e78529453979fddd7dd51796a57ef2f34f8b
Alan <alan@Contrasenas-MacBook-Pro.local>
2022-08-16 17:37:31 -0300

    Responsive adaptation started

--- src/components/biography/Biography.js
+++ src/components/biography/Biography.js
@@ -7,21 +7,23 @@ function Biography() {
     <div id='About'>
       <div className='bio'>
         <h3>About Me</h3>
-        <p>Work for money and code for love. <br/> I'm new web developer guided and educated by a senior one, trained personally and in courses in pages like FreeCodeCamp.</p>
+        <p>Work for money and code for love. <br/> I'm a new web developer guided and educated by a senior one, trained personally and in courses in pages like FreeCodeCamp.</p>
       </div>
       <hr />
       <div className='tecnologies'>
         <h4 className='tec-title'>Most applied technologies</h4>
         <div className='tec-items'>
-          <ul>
-            {/* <li><i className="fa-brands fa-html5"> HTML 5</i></li> */}
-            <li><i className="fa-brands fa-react"> React</i></li>
-            <li><i className="fa-brands"><img src='./imgs/nextjs.png' alt='next.js-icon'/> Next.js</i></li> 
-            <li><i className="fa-brands fa-js"> JavaScript</i></li>
-            <li><i className="fa-brands fa-node-js"> Node.js</i></li>
-            {/* <li><i className="fa-brands fa-sass"> Sass</i></li> */}
-            <li><i className="fa-brands fa-css3-alt"> CSS</i></li>
-            <li><i className="fa-brands fa-bootstrap"> Bootstrap</i></li>
+          <ul className='tec-list'>
+            <div className='tec-list-item'>
+              <li><i className="fa-brands fa-react"> React</i></li>
+              <li><i className="fa-brands fa-html5"> Html5</i></li>
+              <li><i className="fa-brands fa-js"> JavaScript</i></li>
+            </div>
+            <div className='tec-list-item'>
+              <li><i className="fa-brands fa-node-js"> Node.js</i></li>
+              <li><i className="fa-brands fa-css3-alt"> CSS</i></li>
+              <li><i className="fa-brands fa-bootstrap"> Bootstrap</i></li>
+              </div>
           </ul>
         </div>
       </div>
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import "./Home.css";
-import Photo from "./perfil-photo.jpeg";
 
 
 function Home() {
   return (
     <div className='home-container' id='Home'>
-      <img id="photo" src={Photo} alt="Alan Silva" />
+      <img id="photo" src="./imgs/perfil-photo.jpeg" alt="Alan Silva" />
       <div className="presentation-section">
         <h2>Hi! I am Alan Silva</h2>
         <p>And this is my Portfolio <br/> Thanks for visiting it!</p>
       </div>
       <div className="contact-section">
-        <p className='contact-title'>Contact Me</p>
+        <h4 className='contact-title'>Contact Me</h4>
         <p>alansilvanfuerza@gmail.com</p>
         <p>+598 92 756 833</p>
         <p>Montevideo, Uruguay</p>
