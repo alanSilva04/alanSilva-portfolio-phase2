0c3720a4b602e2c7fcd9008bcd6355de31ee970f
Alan <alan@Contrasenas-MacBook-Pro.local>
2022-08-25 11:27:03 -0300

    Added first project

--- src/components/proyects/proyects-data/data.js
+++ src/components/proyects/proyects-data/data.js
@@ -11,27 +11,27 @@ export default [
         ],
         id: 1
     },
-    {
-        title:"Car deals search engine",
-        description: "Search cars for sale accross mercadolibre and facebook marketplace in one place",
-        url:"google.com",
-        image:"./imgs/Mugis.jpg",
-        techs:[
-            "React.js",
-            "Next.js",
-        ],
-        id: 2
-    },
-    {
-        title:"Car deals search engine",
-        description: "Search cars for sale accross mercadolibre and facebook marketplace in one place",
-        url:"google.com",
-        image:"./imgs/Kaido.jpeg",
-        techs:[
-            "React.js",
-            "Next.js",
-            "Bootstrap",
-        ],
-        id: 3
-    },
+    // {
+    //     title:"Car deals search engine",
+    //     description: "Search cars for sale accross mercadolibre and facebook marketplace in one place",
+    //     url:"google.com",
+    //     image:"./imgs/Mugis.jpg",
+    //     techs:[
+    //         "React.js",
+    //         "Next.js",
+    //     ],
+    //     id: 2
+    // },
+    // {
+    //     title:"Car deals search engine",
+    //     description: "Search cars for sale accross mercadolibre and facebook marketplace in one place",
+    //     url:"google.com",
+    //     image:"./imgs/Kaido.jpeg",
+    //     techs:[
+    //         "React.js",
+    //         "Next.js",
+    //         "Bootstrap",
+    //     ],
+    //     id: 3
+    // },
 ]
