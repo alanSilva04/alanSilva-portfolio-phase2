5336a430dea865e47b4bf74450db7b62ead7ee07
Alan <alan@Contrasenas-MacBook-Pro.local>
2022-08-26 12:53:15 -0300

    Chat Project Added

--- src/components/proyects/proyects-data/data.js
+++ src/components/proyects/proyects-data/data.js
@@ -11,17 +11,17 @@ export default [
         ],
         id: 1
     },
-    // {
-    //     title:"Car deals search engine",
-    //     description: "Search cars for sale accross mercadolibre and facebook marketplace in one place",
-    //     url:"google.com",
-    //     image:"./imgs/Mugis.jpg",
-    //     techs:[
-    //         "React.js",
-    //         "Next.js",
-    //     ],
-    //     id: 2
-    // },
+    {
+        title:"Socket.io Chat",
+        description: "My first attempt to make a functional multidevice chat aplicatión deploying the API in Heroku",
+        url:"https://chat-project-as.vercel.app/",
+        image:"./imgs/chatProject.jpg",
+        techs:[
+            "React.js",
+            "Socket.io",
+        ],
+        id: 2
+    },
     // {
     //     title:"Car deals search engine",
     //     description: "Search cars for sale accross mercadolibre and facebook marketplace in one place",
