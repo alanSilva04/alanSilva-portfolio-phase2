232d4a29ca8635ca3041ef123e3d5db4fac9962a
Alan <alan@Contrasenas-MacBook-Pro.local>
2022-09-01 16:09:44 -0300

    Third Project Added

--- src/components/biography/Biography.js
+++ src/components/biography/Biography.js
@@ -15,14 +15,14 @@ function Biography() {
         <div className='tec-items'>
           <ul className='tec-list'>
             <div className='tec-list-item'>
-              <li><i className="fa-brands fa-react"> React</i></li>
-              <li><i className="fa-brands fa-html5"> Html5</i></li>
-              <li><i className="fa-brands fa-js"> JavaScript</i></li>
+              <li><i className="fa-brands fa-react"></i> React</li>
+              <li><i className="fa-brands fa-html5"></i> Html5</li>
+              <li><i className="fa-brands fa-js"></i> JavaScript</li>
             </div>
             <div className='tec-list-item'>
-              <li><i className="fa-brands fa-node-js"> Node.js</i></li>
-              <li><i className="fa-brands fa-css3-alt"> CSS</i></li>
-              <li><i className="fa-brands fa-bootstrap"> Bootstrap</i></li>
+              <li><i className="fa-brands fa-node-js"></i> Node.js</li>
+              <li><i className="fa-brands fa-css3-alt"></i> CSS</li>
+              <li><i className="fa-brands fa-bootstrap"></i> Bootstrap</li>
               </div>
           </ul>
         </div>
